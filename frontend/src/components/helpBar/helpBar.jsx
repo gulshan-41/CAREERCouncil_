7fd937546e13321d99b36187ec7e41b23b00740a
deepak-9528 <deepakserela51@gmail.com>
2025-05-13 12:23:11 +0530

    +helpBar file.

---
+++ frontend/src/components/helpBar/helpBar.jsx
@@ -0,0 +1,15 @@
+import "./helpBar.scss";
+
+function HelpBar() {
+    return (
+        <div className="helpbar-main">
+            <div className="utility-section">
+                <div className="help-contact">
+                    Get Free Career Counselling from CAREER Council_: Talk to us at XXXXX-YYYYY
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default HelpBar;
